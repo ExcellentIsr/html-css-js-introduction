d2a93ba4404edb46f445b53516a93d7d6b947964
Danil Katz <i_like_it74@mail.ru>
2023-05-23 23:35:28 +0300

    com

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 //what's wrong
 function sleep(timeout) {
-    return new Promise(resolve => setTimeout(() => resolve(), timeout));
+    return new Promise(resolve => setTimeout(() => resolve(), timeout))
+
+
 }
 function f1() {
-    console.log('f1 performed');
+    console.log('f1 performed')
 }
 function f2() {
-    console.log('f2 performed');
+    console.log('f2 performed')
 }
 function f3() {
-    console.log('f3 performed');
+    console.log('f3 performed')
 }
 // const promise = sleep(2000);
 // promise.then(() => f1()).then(() => f2()).then(() => f3())
@@ -18,22 +20,43 @@ function getId(predicate) {
     const index = ids.findIndex(predicate);
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            return index < 0 ? reject('id not found') : resolve(ids[index]);
+            return index < 0 ? reject('id not found') : resolve(ids[index])
         }, 1000);
-    });
+    })
 }
 function getCar(id) {
     const cars = {
         '123': 'suzuki',
         '124': 'hunday',
-        '125': 'honda',
+        '125': 'honda'
     }
     const car = cars[id];
     return new Promise((resolve, reject) =>
-        setTimeout(() => car ? resolve(car) : reject('no car found'), 1000));
+        setTimeout(() => car ? resolve(car) : reject('no car found'), 1000))
+
 }
 function displayCar(predicate) {
     return getId(predicate)
         .then(id => getCar(id))
-        .then(car => console.log(car));
+
+        .then(car => console.log(car))
+        .catch(error => {
+            console.log(error);
+            //return 'mersedes'
+        })
+
+
 }
+async function displayCar(predicate) {
+    await sleep(20000)
+    try {
+        const id = await getId(predicate)
+        const car = await getCar(id);
+        console.log(car);
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+displayCar(id => id == 126).then(() => console.log('thanks & good bye'));
+console.log('waiting for the data ...')
