1782849716be552d24102173c3106ee6845b9165
Danil Katz <i_like_it74@mail.ru>
2023-05-29 13:59:44 +0300

    12

---
+++ src/UI/EmployeeForm.js
@@ -0,0 +1,17 @@
+export default class EmployeeForm {
+    #buttonElement;
+    #parentElement;
+    constructor(parentId) {
+        this.#parentElement = document.getElementById(parentId);
+        this.#fillSection();
+        this.#buttonElement = document.getElementById('button-id');
+    }
+    #fillSection() {
+        this.#parentElement.innerHTML ='<button id="button-id">Add random employee data</button>';
+    }
+    buttonHasPressed() {
+        return new Promise(resolved => {
+            this.#buttonElement.onclick = () => resolved();
+        });
+    }   
+}
